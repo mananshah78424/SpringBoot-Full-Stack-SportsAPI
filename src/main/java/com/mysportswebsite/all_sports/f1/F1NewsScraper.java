18981c5103c8965589de32d4a76e9d698983b6fa
mananshah78424 <manan78424@gmail.com>
2024-09-27 22:44:49 -0700

    Added F1 Main page News Scraping

--- src/main/java/com/mysportswebsite/all_sports/f1/F1NewsScraper.java
+++ src/main/java/com/mysportswebsite/all_sports/f1/F1NewsScraper.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.Data;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -37,24 +38,57 @@ public class F1NewsScraper {
             options.addArguments("--user-data-dir=/tmp/chrome-user-data");
             driver = new ChromeDriver(options);
 
-            driver.get("https://www.formula1.com/en/latest/all");
+            driver.get("https://www.skysports.com/f1");
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("figcaption p")));
-
-            List<WebElement> articleElements = driver.findElements(By.cssSelector("ul.grid.grid-cols-1 li"));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("news-list")));
+            List<WebElement> articleElements = driver.findElements(By.className("news-list__item"));
             System.out.println(articleElements.size());
-            int articleCount = Math.min(articleElements.size(), 6);  // To ensure we don't exceed the list size
-            for (int i = 0; i < articleCount; i++) {
-                WebElement articleElement = articleElements.get(i);
+            for(WebElement element:articleElements){
+                WebElement imageElement = element.findElement(By.cssSelector("img.news-list__image"));
+                String imageUrl = imageElement.getAttribute("data-src");
+                if (imageUrl == null || imageUrl.isEmpty()) {
+                    // Fallback to src if data-src is empty or not present
+                    imageUrl = imageElement.getAttribute("src");
+                }
 
-                // Extracting the title, link, and image
-                String title = articleElement.findElement(By.cssSelector("figcaption p")).getText();
-                String link = articleElement.findElement(By.cssSelector("a")).getAttribute("href");
-                String imageSrc = articleElement.findElement(By.cssSelector("img")).getAttribute("src");
 
-                NewsArticle article = new NewsArticle(title, link, imageSrc);
+
+                WebElement titleElement = element.findElement(By.cssSelector(".news-list__headline-link"));
+                // Debug: Print the inner HTML to check if the element contains the title text
+                String innerHtml = titleElement.getAttribute("innerHTML");
+                System.out.println("Inner HTML: " + innerHtml);
+
+                // Use JavaScript Executor to get the text content if getText() fails
+                JavascriptExecutor js = (JavascriptExecutor) driver;
+                String title = (String) js.executeScript("return arguments[0].innerText;", titleElement);
+
+                // Trim the title to remove any leading/trailing spaces
+                title = title.trim();
+
+
+                // Extract the description
+                WebElement descriptionElement = element.findElement(By.cssSelector(".news-list__snippet"));
+                String description = descriptionElement.getText();
+
+                // Extract the anchor tag (a tag) to get the link for the article
+                WebElement linkElement = element.findElement(By.cssSelector(".news-list__headline-link"));
+                String articleLink = linkElement.getAttribute("href");
+
+                NewsArticle article = new NewsArticle(title,articleLink,imageUrl);
                 articles.add(article);
             }
+            //int articleCount = Math.min(articleElements.size(), 6);  // To ensure we don't exceed the list size
+//            for (int i = 0; i < articleCount; i++) {
+//                WebElement articleElement = articleElements.get(i);
+//
+//                // Extracting the title, link, and image
+//                String title = articleElement.findElement(By.className("figcaption p")).getText();
+//                String link = articleElement.findElement(By.className("a")).getAttribute("href");
+//                String imageSrc = articleElement.findElement(By.className("img")).getAttribute("src");
+//
+//                NewsArticle article = new NewsArticle(title, link, imageSrc);
+//                articles.add(article);
+//            }
 
         } catch (Exception e) {
             throw new RuntimeException(e);
